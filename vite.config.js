1f82c03dc5855eda6abd85799b99f3c65f829952
ihsanyouns3000 <dr.ihsanyouns@gmail.com>
2025-11-20 11:41:14 -0500

    Updated build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "/web/",
+  base:"/web/",
   build:{
     outDir: "./docs",
   },
