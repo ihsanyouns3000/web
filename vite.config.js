65b22cf649e6124ef7c2c530bab76dcbecfbcdc7
ihsanyouns3000 <dr.ihsanyouns@gmail.com>
2025-11-20 11:37:15 -0500

    base url added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: "/web/",
   build:{
     outDir: "./docs",
   },
 })
+ 
